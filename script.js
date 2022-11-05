ac7d484e74c703b5e13a8dbd15ea2b29ade14dec
Shanel Hickman-Davis <shanelhickmandavis@gmail.com>
2022-11-05 18:43:53 -0400

    instructions added

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
 let guessBtn = document.getElementById("guessBtn");
 let guess = document.getElementById("guess");
 let result = document.getElementById("result");
-let range= document.getElementById("range")
+let instructions= document.getElementById("instructions")
 let userGuess;
-let limit;
+let max;
 let randomNum;
 let guesses = document.getElementById('guesses');
 let guessArray = [];
+let limitSet = false;
 
-
+//set a maximum number
 let setLimit = () =>{
-    console.log(document.getElementById('limit').value)
-    limit = Math.ceil(Number(document.getElementById('limit').value))
-    let isOverOne = (limit >= 1)
-    let isNum = (!isNaN(limit))
+    console.log('max:');
+    console.log(document.getElementById('max').value)
+    max = Math.ceil(Number(document.getElementById('max').value))
+    let isOverOne = (max >= 1)
+    let isNum = (!isNaN(max))
     if (isOverOne && isNum){
-        range.innerText= `Choose a number between 1 and ${Math.ceil(Number(document.getElementById('limit').value))}`
+        console.log('valid num')
+        instructions.innerHTML= `Choose a number between 1 and ${Math.ceil(Number(document.getElementById('max').value))}`
     }else{
-        range.innerHTML= 'Choose a valid number.'
+        instructions.innerHTML= 'Choose a valid number.'
     }
-    randomNum = Math.ceil(Math.random()*limit);
+    randomNum = Math.ceil(Math.random()*max);
+    console.log(randomNum)
+    limitSet=true;
 }
 
 
-
+//guess a number
 const guessNum = () => {
     //set userGuess based on input
    userGuess =  Number(document.getElementById('guess').value);
    //check for safeNumber
     if(!typeof userGuess == 'Number') {
         result.innerHTML = 'That is not a number!'
-    }else if (userGuess > limit || !userGuess > 1) {
+    }else if (userGuess > max || userGuess < 1) {
         result.innerHTML = 'That number is not in range, try again.'
     } else{
         //add to array
+        if(!guessArray.includes(userGuess)){
         guessArray.push(userGuess);
-        guesses.innerText = guessArray;
+        console.log(guessArray)
+        guesses.innerHTML = guessArray;
 //Once safe compare userGuess with randomNum
     if(userGuess == randomNum){
         result.innerHTML= 'You got it!'
@@ -48,12 +55,12 @@ const guessNum = () => {
 
 }    
 }
+    
+}
 
 guessBtn.addEventListener("click", () => {
-    
     setLimit()
-    console.log(`Limit: ${limit}`)
-    console.log(`Random Number: ${randomNum}`)
+    console.log(`Maximum: ${max}`)
     guessNum();
     console.log(`Guess: ${userGuess}`)
 
