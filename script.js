db794d6f4a228e3028b374cc0b386496ae32c179
Shanel Hickman-Davis <shanelhickmandavis@gmail.com>
2022-11-05 21:35:14 -0400

    game logic updates

--- script.js
+++ script.js
@@ -1,67 +1,83 @@
 let guessBtn = document.getElementById("guessBtn");
 let guess = document.getElementById("guess");
 let result = document.getElementById("result");
-let instructions= document.getElementById("instructions")
+let instructions = document.getElementById("instructions");
+let instructions2 = document.getElementById("instructions2");
 let userGuess;
 let max;
 let randomNum;
-let guesses = document.getElementById('guesses');
+let guesses = document.getElementById("guesses");
 let guessArray = [];
-let limitSet = false;
+let randomSet = false;
 
 //set a maximum number
-let setLimit = () =>{
-    console.log('max:');
-    console.log(document.getElementById('max').value)
-    max = Math.ceil(Number(document.getElementById('max').value))
-    let isOverOne = (max >= 1)
-    let isNum = (!isNaN(max))
-    if (isOverOne && isNum){
-        console.log('valid num')
-        instructions.innerHTML= `Choose a number between 1 and ${Math.ceil(Number(document.getElementById('max').value))}`
-    }else{
-        instructions.innerHTML= 'Choose a valid number.'
-    }
-    randomNum = Math.ceil(Math.random()*max);
-    console.log(randomNum)
-    limitSet=true;
+let setMax = () => {
+ if (!randomSet) {
+	 max = parseInt(document.getElementById("maxField").value);
+	  // console.log(`Max: ${max}`);
+	
+	  let isOverOne = max >= 1;
+	  let isNum = !isNaN(max);
+	  if (isOverOne && isNum) {
+	    console.log("Received a valid num");
+	    instructions.innerHTML = `Choose a number between 1 and ${max}`;
+	  } else {
+	    instructions.innerHTML = "Choose a valid number.";
+	  }
+} else {
+	
 }
-
+};
+getRandom = () => {
+  if (!randomSet) {
+    randomNum = Math.ceil(Math.random() * max);
+    console.log(`Random Number is ${randomNum}`);
+    randomSet = true;
+  }
+};
 
 //guess a number
 const guessNum = () => {
-    //set userGuess based on input
-   userGuess =  Number(document.getElementById('guess').value);
-   //check for safeNumber
-    if(!typeof userGuess == 'Number') {
-        result.innerHTML = 'That is not a number!'
-    }else if (userGuess > max || userGuess < 1) {
-        result.innerHTML = 'That number is not in range, try again.'
-    } else{
-        //add to array
-        if(!guessArray.includes(userGuess)){
-        guessArray.push(userGuess);
-        console.log(guessArray)
-        guesses.innerHTML = guessArray;
+  //reset instruction field 2
+  instructions2.innerHTML = "";
+  result.innerHTML = ''
+  //set userGuess based on input
+  userGuess = Math.ceil(Number(document.getElementById("guessField").value));
+  //check for safeNumber
+  if (!typeof userGuess == "Number") {
+    result.innerHTML = "That is not a number!";
+  } else if (userGuess > max || userGuess < 1) {
+    instructions2.innerHTML = "That number is not in range, try again.";
+  } 
+  else {
+    //add to array if not in array and not a fraction
+    !guessArray.includes(userGuess)
+      ? guessArray.push(userGuess)
+      : instructions2.innerHTML = 'You already guessed this number';
+  }
+  guesses.innerHTML = guessArray;
+};
 //Once safe compare userGuess with randomNum
-    if(userGuess == randomNum){
-        result.innerHTML= 'You got it!'
-    }
-    else if (userGuess < randomNum) {
-        result.innerHTML= 'No, try a higher number'
-    } else if(userGuess > randomNum) {
-        result.innerHTML= 'No, try a lower number'
-    }
 
-}    
-}
+const compareGuesses = () => {
+if(inRange){
+    if (userGuess == randomNum) {
+        result.innerHTML = `You got it! It only took ${guessArray.length} tries`;
+      } else if (userGuess < randomNum) {
+        result.innerHTML = "No, try a higher number";
+      } else if (userGuess > randomNum) {
+        result.innerHTML = "No, try a lower number";
+      }
+} else{
     
 }
+};
 
 guessBtn.addEventListener("click", () => {
-    setLimit()
-    console.log(`Maximum: ${max}`)
-    guessNum();
-    console.log(`Guess: ${userGuess}`)
-
+  setMax();
+  console.log(`Maximum: ${max}`);
+  getRandom();
+  guessNum();
+  console.log(`Guess: ${userGuess}`);
+  compareGuesses();
 });
